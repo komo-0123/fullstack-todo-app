9b0cb4a3b7a0c6b3775edd926aea327406d48727
kotarokomori <komopro.works@gmail.com>
2024-12-12 23:09:53 +0900

    Todo更新用と削除用の関数を追加

--- backend/app/main.go
+++ backend/app/main.go
@@ -62,6 +62,54 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(newTodo)
 }
 
+// TodoリストのIDを指定して更新する
+func updateTodoById(w http.ResponseWriter, r *http.Request) {
+    idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        http.Error(w, "Invalid ID", http.StatusBadRequest)
+        return
+    }
+
+    var updatedTodo Todo
+    if err := json.NewDecoder(r.Body).Decode(&updatedTodo); err != nil {
+        http.Error(w, "Invalid input", http.StatusBadRequest)
+        return
+    }
+
+    for i, todo := range todos {
+        if todo.Id == id {
+            todos[i] = updatedTodo
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(updatedTodo)
+            return
+        }
+    }
+}
+
+// TodoリストのIDを指定して削除する
+func deleteTodoById(w http.ResponseWriter, r *http.Request) {
+    idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        http.Error(w, "Invalid ID", http.StatusBadRequest)
+        return
+    }
+
+    for i, todo := range todos {
+        if todo.Id == id {
+            todos = append(todos[:i], todos[i+1:]...)
+            
+            w.Header().Set("Content-Type", "application/json")
+            w.WriteHeader(http.StatusOK)
+            json.NewEncoder(w).Encode(todos)
+            return
+        }
+    }
+
+    http.Error(w, "Todo not found", http.StatusNotFound)
+}
+
 func main() {
     fmt.Println("Hello Go!")
 }
